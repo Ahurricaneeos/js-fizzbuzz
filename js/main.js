d93a0b75ba0589b6c848a2acbbe7b47be5080e39
Ahurricaneeos <aurora_sala@hotmail.it>
2023-03-30 17:40:14 +0200

    Added html and style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 for (let i=1; i <= 100; i++)
 {
-    if (i % 15 == 0)
+    if (i % 15 == 0){
+    document.getElementById("container-for-squares").innerHTML += `
+    <div class= "fizzbuzz">
+        <p>FizzBuzz</p>
+    </div>
+    `;
         console.log("FizzBuzz");
-    else if (i % 3 == 0)
+    } else if (i % 3 == 0){
+    document.getElementById("container-for-squares").innerHTML += `
+    <div class= "fizz">
+        <p>FizzBuzz</p>
+    </div>
+    `;
         console.log("Fizz");
-    else if (i % 5 == 0)
+    } else if (i % 5 == 0){
+    document.getElementById("container-for-squares").innerHTML += `
+    <div class= "buzz">
+        <p>FizzBuzz</p>
+    </div>
+    `;
         console.log("Buzz");
-    else
+    } else {
+    document.getElementById("container-for-squares").innerHTML += `
+    <div class= "none">
+        <p>${i}</p>
+    </div>
+    `;
         console.log(i);
+    }
 }
